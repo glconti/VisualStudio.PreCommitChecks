d35a3c3aaf759f20c80590470074f3b3f68e8d62
Gianluigi Conti <gianluigi.conti@outlook.com>
2016-04-18 19:00:54 +0200

    Remove references to Microsoft.TeamFoundation

--- src/Command/CleanupDirtyFilesCommand.cs
+++ src/Command/CleanupDirtyFilesCommand.cs
@@ -1,40 +1,36 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="CleanupDirtyFilesCommand.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
+﻿#region using
 
 using System;
 using System.ComponentModel.Design;
-using System.Linq;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.TeamFoundation.Git.Extensibility;
+
+#endregion
 
 namespace VSPreCommitChecks.Command
 {
     /// <summary>
-    /// Command handler
+    ///   Command handler
     /// </summary>
     internal sealed class CleanupDirtyFilesCommand
     {
         /// <summary>
-        /// Command ID.
+        ///   Command ID.
         /// </summary>
         public const int CommandId = 0x0100;
 
         /// <summary>
-        /// Command menu group (command set GUID).
+        ///   Command menu group (command set GUID).
         /// </summary>
         public static readonly Guid CommandSet = new Guid("476c4c16-bfa5-4e13-9e55-3b213cefa85e");
 
         /// <summary>
-        /// VS Package that provides this command, not null.
+        ///   VS Package that provides this command, not null.
         /// </summary>
         private readonly Package package;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CleanupDirtyFilesCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
+        ///   Initializes a new instance of the <see cref="CleanupDirtyFilesCommand" /> class.
+        ///   Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
         private CleanupDirtyFilesCommand(Package package)
@@ -56,27 +52,20 @@ namespace VSPreCommitChecks.Command
         }
 
         /// <summary>
-        /// Gets the instance of the command.
+        ///   Gets the instance of the command.
         /// </summary>
-        public static CleanupDirtyFilesCommand Instance
-        {
-            get;
-            private set;
-        }
+        public static CleanupDirtyFilesCommand Instance { get; private set; }
 
         /// <summary>
-        /// Gets the service provider from the owner package.
+        ///   Gets the service provider from the owner package.
         /// </summary>
         private IServiceProvider ServiceProvider
         {
-            get
-            {
-                return this.package;
-            }
+            get { return this.package; }
         }
 
         /// <summary>
-        /// Initializes the singleton instance of the command.
+        ///   Initializes the singleton instance of the command.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
         public static void Initialize(Package package)
@@ -85,21 +74,14 @@ namespace VSPreCommitChecks.Command
         }
 
         /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
+        ///   This function is the callback used to execute the command when the menu item is clicked.
+        ///   See the constructor to see how the menu item is associated with this function using
+        ///   OleMenuCommandService service and MenuCommand class.
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            var gitService = (IGitExt)ServiceProvider.GetService(typeof(IGitExt));
-
-            var repository = gitService.ActiveRepositories.FirstOrDefault();
-
-            if (repository == null) return;
-
-
         }
     }
 }
